773f6dbcd7a4dc5bef07f42c16cf025a8e65a444
Heritiana <randriamalalaheritiana20@gmail.com>
2023-01-27 13:22:01 +0300

    modif min

--- server.js
+++ server.js
@@ -1,34 +1,104 @@
-var server     = require('http').createServer(),
-    io = require('socket.io')(server, {
-        cors: {
-            origin: ['http://192.168.110.110:9229'],
-        }
+const { createServer } = require("http");
+const express = require("express");
+const WebSocket = require("ws");
+var cors = require('cors')
 
-    }),
-    logger     = require('winston'),
-    port       = 3001;
 
-// Logger config
-logger.remove(logger.transports.Console);
-logger.add(logger.transports.Console, { colorize: true, timestamp: true });
-logger.info('SocketIO > listening on port ' + port);
+// Configure express for serving files
+const app = express();
+app.use(cors({credentials: true,origin: '192.168.110.110'}));
+
+app.use(function(req, res, next) {
+    res.setHeader("Access-Control-Allow-Origin", "*");
+    res.setHeader("Access-Control-Allow-Headers", "X-Requested-With");
+    res.setHeader("Access-Control-Allow-Headers", "Content-Type");
+    res.setHeader("Access-Control-Allow-Methods", "PUT, GET, POST, PATCH, DELETE, OPTIONS");  
+    res.setHeader('Access-Control-Allow-Credentials', false);  
+    res.setHeader("Content-Security-Policy", "script-src 'self'");
+    res.send(`HTTPS: ${req.secure}`)
+    next();
+  });
+
+var options = {
+  allowUpgrades: true,
+  allowEIO3: true,
+  allowEIO2: true, 
+  allowEIO1: true, 
+ // transports: ['websocket', 'file', 'htmlfile', 'xhr-polling', 'jsonp-polling', 'polling'],
+  //pingTimeout: 9000,
+  //pingInterval: 3000,
+  //httpCompression: true,
+  origins: '*:*' ,
+  cors: {
+    origin: ["http://localhost","https://ariel-server-socket.glitch.me",'tcp://ariel-server-socket.glitch.me:3000'],
+    methods: ["GET", "POST"],
+    credentials: true,
+    
+  }
+};
+
+
+
+
+
+
+app.use(express.json({ extended: false }));
+app.use(express.static("public"));
+app.get("/", (request, response) => {
+  response.sendFile(__dirname + "/public/driver.html");
+});
+app.get("/robot", (request, response) => {
+  response.sendFile(__dirname + "/public/robot.html");
+});
+
+// Launch express server
+var  port  = process.env.PORT;
+const server = createServer(app);
+const io = require('socket')(server,options)
 io.on('connection', function (socket){
-    var nb = 0;
-    io.emit('user', {name: 'Marcelo Aires'});
+  console.log('connected dfdf ')
+  io.emit('user', {name: 'Marcelo Aires'});
 
-    logger.info('SocketIO > Connected socket ' + socket.id);
+    //logger.info('SocketIO > Connected socket ' + socket.id);
 
     socket.on('broadcast', function (message) {
-        ++nb;
 
-        logger.info('ElephantIO broadcast > ' + JSON.stringify(message));
+       // logger.info('ElephantIO broadcast > ' + JSON.stringify(message));
          io.emit('infos', {name: JSON.stringify(message)});
     });
 
     socket.on('disconnect', function () {
-        logger.info('SocketIO : Received ' + nb + ' messages');
-        logger.info('SocketIO > Disconnected socket ' + socket.id);
+       // logger.info('SocketIO : Received ' + nb + ' messages');
+        //logger.info('SocketIO > Disconnected socket ' + socket.id);
     });
 });
+server.listen(port, () => {
+  console.log(`Server running on port: ${port}`);
+});
 
-server.listen(port);
+// Launch websocket server
+/** const webSocketServer = new WebSocket.Server({ server });
+webSocketServer.on("connection", socket => {
+  console.info("Total connected clients:", webSocketServer.clients.size);
+  app.locals.clients = webSocketServer.clients;
+
+  // Send all messages to all other clients
+  socket.on("message", message => {
+    webSocketServer.clients.forEach(client => {
+      if (client != socket && client.readyState === WebSocket.OPEN) {
+        client.send(message);
+      }
+    });
+  });
+
+  // End call when any client disconnects
+  socket.on("close", () => {
+    webSocketServer.clients.forEach(client => {
+      if (client != socket && client.readyState === WebSocket.OPEN) {
+        client.send(JSON.stringify({ type: "endCall" }));
+      }
+    });
+  });
+  
+  socket.send("Hello from server"); 
+});**/
